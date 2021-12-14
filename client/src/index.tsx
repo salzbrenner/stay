534174659ef86a2d956bbf125b23e10594167c77
Evan Salzbrenner <evan.salzbrenner@gmail.com>
2021-12-13 20:21:51 -0800

    add react router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,19 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
-import { Listings } from "./sections";
+import { Home, Host, Listing, Listings, NotFound, User } from "./sections";
 import "./styles/index.css";
 
 const client = new ApolloClient({
   uri: "/api", // can set instead of localhost:9000/api b/c of proxy in package.json
 });
 
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/host" element={<Host />} />
+        <Route path="/listing/:id" element={<Listing />} />
+        <Route path="/listings/:location?" element={<Listings />} />
+        <Route element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+};
+
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <Listings title="My house listings" />
+      <App />
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
