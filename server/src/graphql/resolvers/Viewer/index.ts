d5cb9e16f3e298990e6662273a5daa6363e82cff
Evan Salzbrenner <evan.salzbrenner@gmail.com>
2021-12-15 21:19:03 -0800

    Google auth resolvers

--- server/src/graphql/resolvers/Viewer/index.ts
+++ server/src/graphql/resolvers/Viewer/index.ts
@@ -1,17 +1,141 @@
+import crypto from "crypto";
 import { IResolvers } from "graphql-tools";
+import { Google } from "../../../lib/api";
+import { Database, User, Viewer } from "../../../lib/types";
+import { LogInArgs } from "./types";
+
+const logInViaGoogle = async (
+  code: string,
+  token: string,
+  db: Database
+): Promise<User | undefined> => {
+  const { user } = await Google.logIn(code);
+
+  if (!user) {
+    throw new Error("Google login error");
+  }
+
+  // Names/Photos/Email lists
+  const userNamesList = user.names && user.names.length ? user.names : null;
+  const userPhotosList = user.photos && user.photos.length ? user.photos : null;
+  const userEmailsList =
+    user.emailAddresses && user.emailAddresses.length
+      ? user.emailAddresses
+      : null;
+
+  // User display Name
+  const userName = userNamesList ? userNamesList[0].displayName : null;
+
+  // user id
+  const userId =
+    userNamesList &&
+    userNamesList[0].metadata &&
+    userNamesList[0].metadata.source
+      ? userNamesList[0].metadata.source.id
+      : null;
+
+  // avatar
+  const userAvatar =
+    userPhotosList && userPhotosList[0].url ? userPhotosList[0].url : null;
+
+  // email
+  const userEmail =
+    userEmailsList && userEmailsList[0].value ? userEmailsList[0].value : null;
+
+  if (!userId || !userName || !userAvatar || !userEmail) {
+    throw new Error("Google login error");
+  }
+
+  const updateRes = await db.users.findOneAndUpdate(
+    {
+      _id: userId,
+    },
+    {
+      $set: {
+        name: userName,
+        avatar: userAvatar,
+        contact: userEmail,
+        token,
+      },
+    },
+    { returnDocument: "after" } // return the updated document, NOT the original
+  );
+
+  let viewer = updateRes.value ? updateRes.value : undefined;
+
+  if (!viewer) {
+    const insertResult = await db.users.insertOne({
+      _id: userId,
+      token,
+      name: userName,
+      avatar: userAvatar,
+      contact: userEmail,
+      income: 0,
+      bookings: [],
+      listings: [],
+    });
+
+    const insertedId = insertResult.insertedId;
+    const userQuery = await db.users.findOne({
+      _id: insertedId,
+    });
+
+    viewer = userQuery ? userQuery : undefined;
+  }
+  return viewer;
+};
 
 export const viewerResolvers: IResolvers = {
   Query: {
     authUrl: () => {
-      return "Query.authUrl";
+      try {
+        return Google.authUrl;
+      } catch (error) {
+        throw new Error(`Failed to query Google Auth Url: ${error}`);
+      }
     },
   },
   Mutation: {
-    logIn: () => {
-      return "Mutation.logIn";
+    // context ({db}) is 3rd positional arg!!
+    logIn: async (
+      _root: undefined,
+      { input }: LogInArgs,
+      { db }: { db: Database }
+    ): Promise<Viewer> => {
+      try {
+        const code = input ? input.code : null;
+        const token = crypto.randomBytes(16).toString("hex");
+
+        const viewer: User | undefined = code
+          ? await logInViaGoogle(code, token, db)
+          : undefined;
+
+        if (!viewer) {
+          return { didRequest: true };
+        }
+
+        return {
+          _id: viewer._id,
+          token: viewer.token,
+          avatar: viewer.avatar,
+          walletId: viewer.walletId,
+          didRequest: true,
+        };
+      } catch (error) {
+        throw new Error(`Failed to log in: ${error}`);
+      }
     },
-    logOut: () => {
-      return "Mutation.logout";
+    logOut: (): Viewer => {
+      try {
+        return { didRequest: true };
+      } catch (error) {
+        throw new Error(`Failed to log out: ${error}`);
+      }
     },
+  },
+  Viewer: {
+    id: (viewer): string | undefined => viewer._id,
+    hasWallet: (viewer: Viewer): boolean | undefined =>
+      viewer.walletId ? true : undefined,
   },
 };
--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -1,5 +1,13 @@
 import { Collection, ObjectId } from "mongodb";
 
+export interface Viewer {
+  _id?: string;
+  token?: string;
+  avatar?: string;
+  walletId?: string;
+  didRequest: boolean;
+}
+
 export interface BookingsIndexMonth {
   [key: string]: boolean;
 }
