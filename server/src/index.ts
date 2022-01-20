7564c22f295049ce7061d90619392ab320ae9af4
Evan Salzbrenner <evan.salzbrenner@gmail.com>
2022-01-19 18:01:53 -0800

    fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,5 @@
-if (process.env.NODE_ENV === "development") {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  require("dotenv").config();
-}
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require("dotenv").config();
 
 import express, { Application } from "express";
 import cookieParser from "cookie-parser";
